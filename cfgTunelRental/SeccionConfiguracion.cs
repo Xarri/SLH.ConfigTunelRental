802fbf460d58f77baed057e09ca9ce7c621ecef1
Pablo Gonzalez <pablo.valliniello@gmail.com>
2023-11-01 02:12:21 +0100

    Punto inicial

---
+++ cfgTunelRental/SeccionConfiguracion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cfgTunelRental
+{
+    /// <summary>
+    /// Clase de atributos para la seccion CONFIGURACION.
+    /// </summary>
+    /// Autor: Pablo Glez.
+    /// Fecha: 06/09/2023
+    class SeccionConfiguracion
+    {
+        /// <summary>
+        /// Nombre de la sección.
+        /// </summary>
+        private const string NOMBRE_SECCION = "CONFIGURACION";
+
+        /// <summary>
+        /// Código de la máquina
+        /// </summary>
+        public string codigoMaquina = "";
+
+        /// <summary>
+        /// Idioma
+        /// </summary>
+        public string idioma = "es-ES";
+
+        /// <summary>
+        /// Nivel log programa
+        /// </summary>
+        public int nivelLog = 3;
+
+        #region DEFINICIONES RFID
+        /// <summary>
+        /// IP del lector RFID.
+        /// </summary>
+        public string lectorRFID = "";
+
+        /// <summary>
+        /// Potencia del lector RFID (11 - 31).
+        /// </summary>
+        public int potenciaRFID = 25;
+        #endregion
+
+        #region TIEMPOS
+        
+        #endregion
+
+
+        /// <summary>
+        /// Puerto COM
+        /// </summary>
+        public string puertoCOM = "COM5";
+    }
+}
---
+++ cfgTunelRental/cfgTunelRental.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace cfgTunelRental
+{
+    public class cfgTunelRental
+    {
+        /// <summary>
+        /// Ruta completa y nombre del archivo INI
+        /// </summary>
+        private readonly string pathArchivoINI = $"{System.Reflection.Assembly.GetExecutingAssembly().Location}\\Configuracion\\Config.ini";
+
+        /// <summary>
+        /// Objeto para manejar el archivo ini
+        /// </summary>
+        private SicoClases.Ficheros.clsINIs archivoINI;
+
+        #region CONFIGURACIONES DE LA SECCION [CONFIGURACION]
+
+        #endregion
+
+        /*
+                //==== EQUIPO LECTOR ===================================================================================================            
+                //POTENCIA
+                Selecciona_SicoCombo(cboPotenciaLectura, Program.DefinicionesApp.RFID_Potencia.ToString(), SicontrolesWindowsNET.eTipoBusquedaValor.Texto);
+
+                alfCodigoLector.Text = Program.DefinicionesApp.RFID_Maquina;
+                    alfLectorRFID.Text = Program.DefinicionesApp.RFID_LectorRFID;
+                    Selecciona_SicoCombo(cboCOM, Program.DefinicionesApp.CONFIG_PuertoCom, SicontrolesWindowsNET.eTipoBusquedaValor.Texto);
+                cboIdioma.Items.Clear();
+                    cboIdioma.Items.Add(new SicontrolesWindowsNET.General.clsComboBoxItem("es-ES", "es-ES"));
+                    Selecciona_SicoCombo(cboIdioma, Program.DefinicionesApp.CONFIG_Idioma, SicontrolesWindowsNET.eTipoBusquedaValor.Texto);
+                chkCarro.Checked = Program.DefinicionesApp.CONFIG_IdentificarCarro;
+                    alfTiempoLectura.Text = Program.DefinicionesApp.RFID_TiempoLectura.ToString();
+                    AlfElectronicaVer.Text = Program.DefinicionesApp.ELECTRONICA_VERSION.ToString();
+                    Selecciona_SicoCombo(cboPuerta, Program.DefinicionesApp.ELECTRONICA_PUERTA.ToString(), SicontrolesWindowsNET.eTipoBusquedaValor.Texto);
+                chkPuertaUnica.Checked = Program.DefinicionesApp.ELECTRONICA_PUERTA_UNICA;
+                    //======================================================================================================================
+        */
+    }
+}
